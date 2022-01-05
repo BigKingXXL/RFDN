cd5e8e54ba181e483f4663133ef873caf547bb58
Maximilian Schulze <maximilian.schulze@student.hpi.de>
2022-01-05 14:34:11 +0200

    some changes

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import torch
 from utils import utils_logger
 from utils import utils_image as util
 from RFDN import RFDN
+from tqdm import tqdm
 
 
 def main():
@@ -20,11 +21,21 @@ def main():
     testsets = 'DIV2K'
     testset_L = 'DIV2K_valid_LR_bicubic'
     #testset_L = 'DIV2K_test_LR_bicubic'
+    real_L = 'DIV2K_valid_HR'
 
-    torch.cuda.current_device()
-    torch.cuda.empty_cache()
+    if torch.cuda.is_available():
+        torch.cuda.current_device()
+        torch.cuda.empty_cache()
     #torch.backends.cudnn.benchmark = True
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # --------------------------------
+    # read image
+    # --------------------------------
+    L_folder = os.path.join(testsets, testset_L, 'X4')
+    E_folder = os.path.join(testsets, testset_L+'_results')
+    H_folder = os.path.join(testsets, real_L)
+    util.mkdir(E_folder)
 
     # --------------------------------
     # load model
@@ -42,11 +53,46 @@ def main():
     logger.info('Params number: {}'.format(number_parameters))
 
     # --------------------------------
-    # read image
-    # --------------------------------
-    L_folder = os.path.join(testsets, testset_L, 'X4')
-    E_folder = os.path.join(testsets, testset_L+'_results')
-    util.mkdir(E_folder)
+    dynamic = False
+    seqModel = torch.nn.Sequential(torch.quantization.QuantStub(), model, torch.quantization.DeQuantStub())
+    if dynamic:
+        model = torch.quantization.quantize_dynamic(
+            model,  # the original model
+            {torch.nn.Conv2d, torch.nn.BatchNorm2d, torch.nn.InstanceNorm2d, torch.nn.LeakyReLU, torch.nn.ReLU},  # a set of layers to dynamically quantize
+            dtype=torch.quint4)  # the target dtype for quantized weights
+    else:
+
+        seqModel.qconfig = torch.quantization.get_default_qconfig('qnnpack')
+
+        # Fuse the activations to preceding layers, where applicable.
+        # This needs to be done manually depending on the model architecture.
+        # Common fusions include `conv + relu` and `conv + batchnorm + relu`
+        # seqModel = torch.quantization.fuse_modules(seqModel, [])
+        
+
+        # Prepare the model for static quantization. This inserts observers in
+        # the model thats will observe activation tensors during calibration.
+        model_fp32_prepared = torch.quantization.prepare(seqModel)
+
+        # calibrate the prepared model to determine quantization parameters for activations
+        # in a real world setting, the calibration would be done with a representative dataset
+        for index, img in enumerate(tqdm(util.get_image_paths(L_folder))):
+            img_L = util.imread_uint(img, n_channels=3)
+            img_L = util.uint2tensor4(img_L)
+            img_L = img_L.to(device)
+            model_fp32_prepared(img_L)
+            if index == 8:
+                break
+
+        # Convert the observed model to a quantized model. This does several things:
+        # quantizes the weights, computes and stores the scale and bias value to be
+        # used with each activation tensor, and replaces key operators with quantized
+        # implementations.
+        model_int8 = torch.quantization.convert(model_fp32_prepared)
+        model = model_int8
+
+    number_parameters = sum(map(lambda x: x.numel(), model.parameters()))
+    logger.info('Params number: {}'.format(number_parameters))
 
     # record PSNR, runtime
     test_results = OrderedDict()
@@ -56,28 +102,41 @@ def main():
     logger.info(E_folder)
     idx = 0
 
-    start = torch.cuda.Event(enable_timing=True)
-    end = torch.cuda.Event(enable_timing=True)
+    if torch.cuda.is_available():
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+    else:
+        start = time.time()
+        end = time.time()
 
     img_SR = []
-    for img in util.get_image_paths(L_folder):
+    for img in tqdm(util.get_image_paths(L_folder)):
 
         # --------------------------------
         # (1) img_L
         # --------------------------------
         idx += 1
         img_name, ext = os.path.splitext(os.path.basename(img))
-        logger.info('{:->4d}--> {:>10s}'.format(idx, img_name+ext))
+        #logger.info('{:->4d}--> {:>10s}'.format(idx, img_name+ext))
 
         img_L = util.imread_uint(img, n_channels=3)
         img_L = util.uint2tensor4(img_L)
         img_L = img_L.to(device)
 
-        start.record()
+        if torch.cuda.is_available():
+            start.record()
+        else:
+            start = time.time()
         img_E = model(img_L)
-        end.record()
-        torch.cuda.synchronize()
-        test_results['runtime'].append(start.elapsed_time(end))  # milliseconds
+        if torch.cuda.is_available():
+            end.record()
+        else:
+            end = time.time()
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+            test_results['runtime'].append(start.elapsed_time(end))  # milliseconds
+        else:
+            test_results['runtime'].append((end - start) * 1000)
 
         # --------------------------------
         # (2) img_E
@@ -88,7 +147,7 @@ def main():
         # --------------------------------
         # (3) save results
         # --------------------------------
-        #util.imsave(img_E, os.path.join(E_folder, img_name+ext))
+        # util.imsave(img_E, os.path.join(E_folder, img_name+ext))
 
     ave_runtime = sum(test_results['runtime']) / len(test_results['runtime']) / 1000.0
     logger.info('------> Average runtime of ({}) is : {:.6f} seconds'.format(L_folder, ave_runtime))
@@ -96,16 +155,15 @@ def main():
     # --------------------------------
     # (4) calculate psnr
     # --------------------------------
-    '''
     psnr = []
     idx = 0
-    H_folder = '/home/lj/EfficientSR-1.5.0/train/dataset/benchmark/DIV2K_valid/HR/'
-    for img in util.get_image_paths(H_folder):
+    #H_folder = '/home/lj/EfficientSR-1.5.0/train/dataset/benchmark/DIV2K_valid/HR/'
+    for img in tqdm(util.get_image_paths(H_folder)):
         img_H = util.imread_uint(img, n_channels=3)
         psnr.append(util.calculate_psnr(img_SR[idx], img_H))
         idx += 1
     logger.info('------> Average psnr of ({}) is : {:.6f} dB'.format(L_folder, sum(psnr)/len(psnr)))
-    '''
+
 
 if __name__ == '__main__':
 
